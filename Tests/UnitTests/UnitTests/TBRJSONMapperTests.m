cc3b8e70b1943bb5e7842a0a524699ec30888d0a
Nahuel Marisi <nahuelmarisi@gmail.com>
2015-07-28 16:50:55 +0100

    Created initial structure for UnitTests

--- Tests/UnitTests/UnitTests/TBRJSONMapperTests.m
+++ Tests/UnitTests/UnitTests/TBRJSONMapperTests.m
@@ -1,6 +1,6 @@
 //
-//  UnitTestsTests.m
-//  UnitTestsTests
+//  UnitTests.m
+//  UnitTests
 //
 //  Created by Nahuel Marisi on 2015-07-28.
 //  Copyright (c) 2015 TechBrewers. All rights reserved.
