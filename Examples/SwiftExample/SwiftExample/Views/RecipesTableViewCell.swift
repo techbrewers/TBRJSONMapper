55de38cbefa2d0de958781ea231f50676bfe118b
Nahuel Marisi <nahuelmarisi@gmail.com>
2015-09-06 11:19:12 +0100

    Added initial RecipesTableViewController and its cell

---
+++ Examples/SwiftExample/SwiftExample/Views/RecipesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipesTableViewCell.swift
+//  SwiftExample
+//
+//  Created by Nahuel Marisi on 2015-09-06.
+//  Copyright © 2015 TechBrewers. All rights reserved.
+//
+
+import UIKit
+
+class RecipesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
