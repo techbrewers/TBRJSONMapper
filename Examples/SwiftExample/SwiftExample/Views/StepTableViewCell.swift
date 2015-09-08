3832939bf7c0d12dd29e2de79f22caa89c9e8398
Nahuel Marisi <nahuelmarisi@gmail.com>
2015-09-08 19:57:31 +0100

    Added new Step VC and cell

---
+++ Examples/SwiftExample/SwiftExample/Views/StepTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StepTableViewCell.swift
+//  SwiftExample
+//
+//  Created by Nahuel Marisi on 2015-09-06.
+//  Copyright © 2015 TechBrewers. All rights reserved.
+//
+
+import UIKit
+
+class StepTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
