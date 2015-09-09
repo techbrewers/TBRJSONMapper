4ce5f690c701788cd2bc3572d35458ee7e48b64d
Nahuel Marisi <nahuelmarisi@gmail.com>
2015-09-09 08:25:54 +0100

    cleanup

--- Examples/SwiftExample/SwiftExample/Models/Ingredient.swift
+++ Examples/SwiftExample/SwiftExample/Models/Ingredient.swift
@@ -6,6 +6,10 @@
 //  Copyright © 2015 TechBrewers. All rights reserved.
 //
 
+// NB: We don't use the Ingredients in this example's ViewControllers.
+// However I've decided to keep it so you can see how it's possible to have n
+// levels of nested model classes.
+
 import Foundation
 
 @objc class Ingredient: NSObject {
